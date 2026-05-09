88d0c99480db82302c7b9fd15a3246b3325cb1b6
niklasdele <niklasdeleuran@gmail.com>
2026-05-09 22:28:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,9 +16,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # -----------------------------
 IMAGE_URLS = {
     "bank": {
-        "basement": "https://cdn.discordapp.com/attachments/956265784181657700/1502614451537445056/bank_b.png?ex=6a005a42&is=69ff08c2&hm=96deef4e099ac48717171c484e541fb3bef4f1421dc9d17741739fbe4d2a7962&",
-        "1f": "https://cdn.discordapp.com/attachments/956265784181657700/1502614464598511626/bank_f1.png?ex=6a005a45&is=69ff08c5&hm=57d1a07496edd41a9226a0ec7bd57f4151201f3a2b04df437f5bf267524a00a8&",
-        "2f": "https://cdn.discordapp.com/attachments/956265784181657700/1502614480859955250/bank_f2.png?ex=6a005a49&is=69ff08c9&hm=299d79363a25225143a0889368122d213299d2a32c5177138cef17295672e41b&"
+        "basement": "https://cdn.discordapp.com/attachments/1502767854439628800/1502768021653950504/image.png?ex=6a00e948&is=69ff97c8&hm=de22d68da053582c400ca9113c4be26f395ff2ffc4b55aff2a99641f0a7db706&",
+        "1f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502768003593535568/image.png?ex=6a00e944&is=69ff97c4&hm=ee5db0a1c70e7d60745eebbb6925893bcb14aadf5e51a848bbc7ff5cc5f61e2f&",
+        "2f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502767988938379436/image.png?ex=6a00e940&is=69ff97c0&hm=da97adfdd9ebc2601bf42f363297b5adbec74a800b2bcf9cb47518a083b45338&"
     },
     "border": {
         "1f": "https://your-url.com/border_1f.png",
@@ -31,9 +31,9 @@ IMAGE_URLS = {
         "2f": "https://your-url.com/clubhouse_2f.png"
     },
     "chalet": {
-        "basement": "https://cdn.discordapp.com/attachments/956265784181657700/1502632891379748974/chalet_b.png?ex=6a006b6e&is=69ff19ee&hm=6ace4bb9cacf6690da056309f841f751914053aeb1bf76349a2c78931ea78d64&",
-        "1f": "https://cdn.discordapp.com/attachments/956265784181657700/1502632916260487278/chalet_f1.png?ex=6a006b74&is=69ff19f4&hm=a2b36f3a2d3e6b9d239226ea110a4bdcba0a8bd540a8ba08122f1e1cd9ad3d51&",
-        "2f": "https://cdn.discordapp.com/attachments/956265784181657700/1502632935482982440/chalet_f2.png?ex=6a006b79&is=69ff19f9&hm=b1184c7f203e7c46c569ffaec43c9f548caefee86601f350b42119ad2755a277&"
+        "basement": "https://cdn.discordapp.com/attachments/1502767854439628800/1502767968491147344/image.png?ex=6a00e93b&is=69ff97bb&hm=d58c43abf5db2c67f1b680663cd7a0dba91670cf9b6d318302f4e2130759517d&",
+        "1f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502767917173964891/image.png?ex=6a00e92f&is=69ff97af&hm=786ef7af4a3bf89643301a5cc844c131c68a4b764277891df37d3361cdcff047&",
+        "2f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502767908462264361/image.png?ex=6a00e92d&is=69ff97ad&hm=e985d265823fc8a24a474f6ae346e4ea95da8b8340251ba23bd7bb97921604d4&"
     },
     "kafe": {
         "1f": "https://cdn.discordapp.com/attachments/1472963714927038566/1472983907606724750/kafe_f1.png?ex=69fc0c6a&is=69fabaea&hm=53230cb9b1dd9f5ca11fc0eb4cd28c30d4496692866213954556d41d327a67d5&",
@@ -41,9 +41,9 @@ IMAGE_URLS = {
         "3f": "https://cdn.discordapp.com/attachments/1472963714927038566/1472983973557829905/kafe_f3.png?ex=69fc0c7a&is=69fabafa&hm=0a485f339978034cf85780cdda64b54bf3bfa21df71fa42962507294bd2e0fd3&"
     },
     "oregon": {
-        "basement": "https://cdn.discordapp.com/attachments/1472963714927038566/1476946606229159987/oregon_b.png?ex=69fbf677&is=69faa4f7&hm=c9374f526c1caeb1bccd66d9e8e6a4b2bbf58b475b991fe3dedaaf4a63d94106&",
-        "1f": "https://cdn.discordapp.com/attachments/1472963714927038566/1476946644682674307/oregon_f1_1.png?ex=69fbf680&is=69faa500&hm=1c57edb5651b67764998aa9863533596c55c722151f4f4e11212e52478e141f2&",
-        "2f": "https://cdn.discordapp.com/attachments/1472963714927038566/1476946682775081194/oregon_f2_1.png?ex=69fbf689&is=69faa509&hm=54892d4cbdfc272be0f4b9d696d043160da568ebc8205f07289cf48331f04be4&"
+        "basement": "https://cdn.discordapp.com/attachments/1502767854439628800/1502768185450172446/image.png?ex=6a00e96f&is=69ff97ef&hm=cb8de46fc3c52c3ac533785cbf68a5790d7276f1127887d34f7fb722ad5e6938&",
+        "1f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502768173764710571/image.png?ex=6a00e96c&is=69ff97ec&hm=023cb7b6d88dccb1632dd3ca4fdf08f13db430cec3fad3521211aff678dd98ce&",
+        "2f": "https://cdn.discordapp.com/attachments/1502767854439628800/1502768145478455296/image.png?ex=6a00e965&is=69ff97e5&hm=1a4268d001d1aa25e89a3cb03e8dc6a527a00c0b493857b2db1a545f7300dfd8&"
     },
     "consulate": {
         "basement": "https://your-url.com/consulate_basement.png",
@@ -76,13 +76,13 @@ IMAGE_URLS = {
 # FLOOR LISTS
 # -----------------------------
 FLOORS = {
-    "bank": ["basement", "1f", "2f", "roof"],
-    "border": ["1f", "2f", "roof"],
+    "bank": ["basement", "1f", "2f"],
+    "border": ["1f", "2f"],
     "clubhouse": ["basement", "1f", "2f"],
     "chalet": ["basement", "1f", "2f"],
     "kafe": ["1f", "2f", "3f"],
     "oregon": ["basement", "1f", "2f"],
-    "consulate": ["basement", "1f", "2f", "roof"],
+    "consulate": ["basement", "1f", "2f"],
     "skyscraper": ["1f", "2f"],
     "villa": ["1f", "2f"],
     "labs": ["basement", "1f", "2f"],
